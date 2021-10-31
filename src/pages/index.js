05e342ed8a79912fac5098d22fa6380709e2d898
RuBrock <r.brocksilva@yahoo.com.br>
2021-10-31 16:21:22 -0300

    Initializing the app build

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram 2.0 NITRO</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>This is the Instagram 2.0 NITRO Build</h1>
+    </div>
+  );
+}
